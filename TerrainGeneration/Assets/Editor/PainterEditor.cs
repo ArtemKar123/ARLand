7245e891c2d5c78dd0bd2ef82ffbe835f1524b78
ArtemKar123 <wartemtem@yandex.ru>
2020-08-09 15:10:28 +0800

    new repo created

---
+++ TerrainGeneration/Assets/Editor/PainterEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+[CustomEditor(typeof(Painter))]
+public class PainterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        Painter genScript = (Painter) target;
+        if (GUILayout.Button("Paint"))
+        {
+            genScript.Paint();
+        }
+    }
+}
